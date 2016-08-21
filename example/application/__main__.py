f6731afbdb6b57010fe6471b22d9820bfb77abc5
Marcell Mars <ki.ber@kom.uni.st>
2016-08-21 17:37:42 +0200

    added snipdom skeleton with ipython

--- example/application/__main__.py
+++ example/application/__main__.py
@@ -1,28 +1,78 @@
-import tkinter as tk
+from PyQt5.Qt import (Qt,
+                      QObject,
+                      QMainWindow,
+                      QSplitter,
+                      pyqtSignal,
+                      QPushButton,
+                      QApplication)
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+from IPython.qt.inprocess import QtInProcessKernelManager
+from IPython.lib import guisupport
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
+import sys
 
-    def say_hi(self):
-        hello = "Hello World\n(click me)"
-        if self.hi_there["text"] == hello:
-            self.hi_there["text"] = 'Hi there!\n(click me)'
-        else:
-            self.hi_there["text"] = hello
+APPNAME = "sniplog"
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+class Js2Py(QObject):
+    sent = pyqtSignal(str)
+
+    def sendToPython(self, s):
+        self.sent.emit(s)
+
+class Snipdom(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+
+        self.hsplit = QSplitter()
+        self.setCentralWidget(self.hsplit)
+
+        kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel()
+        self.kernel = kernel_manager.kernel
+        self.kernel.gui = 'qt'
+
+        self.control = RichIPythonWidget(gui_completion="droplist")
+
+        self.kernel.shell.push({'snipdom': self})
+
+        kernel_client = kernel_manager.client()
+        kernel_client.start_channels()
+
+        self.control.kernel_manager = kernel_manager
+        self.control.kernel_client = kernel_client
+
+        self.vsplit = QSplitter()
+        self.vsplit.setOrientation(Qt.Vertical)
+
+        self.vsplit.addWidget(self.control)
+        self.hsplit.addWidget(self.vsplit)
+
+        self.sendButton = QPushButton("send")
+        #self.sendButton.clicked.connect(self.sendcode)
+        self.vsplit.addWidget(self.sendButton)
+
+        self.bridge = Js2Py()
+        self.bridge.sent.connect(self.codeFromJs)
+
+        #lab = QtGui.QLabel(kernel.shell.history_manager.get_range(start=-1).next()[2])
+
+    def codeFromJs(self, code):
+        self.control.input_buffer = code
+
+if __name__=='__main__':
+    if len(sys.argv) == 1:
+        app = QApplication(sys.argv)
+        app.setApplicationName(APPNAME)
+        main = Snipdom()
+        main.show()
+
+        def stop():
+            app.exit()
+
+        main.onclose = stop
+
+        main.control.exit_requested.connect(stop)
+        guisupport.start_event_loop_qt4(app)
